4733b72e2dde21bf98f3a835564e566b18728239
Hoang Vu <hoangvu.74got@gmail.com>
2025-10-08 20:00:00 +0700

    chuẩn bị deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,20 +3,32 @@ import ListRoutes from './routes/ListRoutes.js';
 import { connectDB } from "./config/DB.js";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 const app = express();
 
 
 //middleware
 app.use(express.json());
-app.use(cors({origin:"http://localhost:5173"}));
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin:"http://localhost:5173"}));
+}
 
 app.use('/api/tasks', ListRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../Frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../Frontend/dist/index.html'));
+    });
+}
 
 connectDB().then(() => {
 app.listen(PORT,() => {
